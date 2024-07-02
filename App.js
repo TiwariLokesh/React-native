c7942ae8052c216852962f87d145d1de45b5cdad
Your Name <lokeshtiwari212@gmail.com>
2024-07-02 06:52:52 +0530

    initialized new components

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {View} from 'react-native';
-import Calculator from './Components/Calculator';
+// import Calculator from './Components/Calculator';
 // import Remote from './Components/Remote';
 // import OtpForm from './Components/OtpForm';
 // import RegistrationForm from './Components/RegistrationForm';
+import Count from './Components/Count';
 // import CV from './Components/CV';
 export default function App() {
   return (
@@ -12,7 +13,8 @@ export default function App() {
       {/* <RegistrationForm /> */}
       {/* <OtpForm/> */}
       {/* <Remote/> */}
-      <Calculator/>
+      {/* <Calculator/> */}
+      <Count/>
     </View>
   )
 }
